fb0fbd13dad67d1bf381167f578fc6dba2fdfad2
Kevin Gann <kevin.gann@rev.com>
2024-07-29 15:54:43 -0400

    Trying to sign-in.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('can sign in', async ({ page }) => {
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.goto('http://dev.rev.com/');
+
+  page.getByRole('link', { name: 'Login' }).click();
+  await expect(page).toHaveTitle(/Login/);
+
+  const account = { username: 'xxxx', password: 'yyyy' };
+  page.getByLabel('Email address').fill(account.username);
+  
+  page.getByRole('button', { name: 'Next' }).click();
+  
+  await expect(page.getByLabel('Password')).toBeVisible();
+  page.getByLabel('Password').fill(account.password);
+  page.getByRole('button', { name: 'Sign in', exact: true }).click();
+
+  await expect(page).toHaveTitle(/.*My Files/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
